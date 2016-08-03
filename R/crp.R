c8b58150b16940974b175f0a4c61d34c31257734
Jacob Carey <JCarey@telligen.com>
2016-08-03 13:37:58 -0400

    Make corrections

--- R/crp.R
+++ R/crp.R
@@ -15,22 +15,22 @@ model <- function() {
     
     # 2 to nth customer
     for (i in 2:n) {
-        # construct probability of seats
-        prob <- m
-        prob[(sum(m) > 0) + 1] <- alpha
+        # increase people at chosen table
+        m[table[i]] <- m[table[i]] + 1
         
         # choose seat
         table[i] ~ dcat(prob[])
         
-        # increase people at chosen table
-        m[table[i]] <- m[table[i]] + 1
+        # construct probability of seats
+        prob <- m
+        prob[(sum(m) > 0) + 1] <- alpha
     }
     
     # first customer sits at first table
-    prob[i] <- 1
+    m[1] <- 1
     
     # baseline distribution
-    for (k in 1:N) {
+    for (k in 1:n) {
         phi[k] ~ dnorm(0, 0.0001)   
     }
     
@@ -44,7 +44,7 @@ model.params <- c("alpha", "theta", "table", "m")
 # fit model
 set.seed(42)
 fit <- jags(model.data, NULL, model.params, model, 
-            n.iter=30000)
+            n.iter=3000)
 
 # diagnose model
 launch_shinystan(as.shinystan(as.mcmc(fit)))
